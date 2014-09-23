54d6a0abd6236ec230d5ac0ee5031b5b768f8108
TsRebornz <macaren@bk.ru>
2014-09-23 20:46:38 +0300

    Topology_node
    Added and testes topology node

--- topology_node.rb
+++ topology_node.rb
@@ -2,12 +2,31 @@
 
 class TopologyNode
 	attr_accessor :node_type
-	attr_accessor :type
 	attr_accessor :model # Ссылка на модель данных
 	attr_accessor :parent_node
 	attr_accessor :children_nodes
 
-	def initialize parent_node
-		
+	def initialize type_node
+		@type_node = type_node
+		@children_nodes = []
 	end
+
+	def to_s
+		puts "type_node #{@type_node}"
+		puts "model #{@model}"
+		puts "parent_node #{@parent_node}"
+		puts "children_nodes #{@children_nodes}"
+	end
+
+	def add_children_node node
+		@children_nodes << node if node
+	end
+
 end
+
+# type_node = :model
+# node = TopologyNode.new(type_node)
+# node.model = "2341324"
+# node.parent_node = "fadsf"
+# node.add_children_node "fadsf"
+# node.to_s
--- topology_tree.rb
+++ topology_tree.rb
@@ -3,22 +3,53 @@ require 'pathname'
 $LOAD_PATH <<  File.dirname(Pathname.new(File.expand_path(__FILE__)).realpath.to_s).to_s
 
 require "topology_node"
+require  'Models/abstract_node_model'
+require  'Models/admin_cluster_model'
+require  'Models/app_server_model'
+require  'Models/database_model'
+require  'Models/host_model'
+
+
+hosts = {
+			:main => { :database => nil, :app_serv => nil, :driver => nil },
+			:reserve => {:database => nil, :app_serv => nil, :driver => nil },
+			:arb => nil,
+			:client1 => nil
+		}
+enter_data = { :where => "ЭЛЕСИ"
+
+
+}
 
 class TopologyTree
 	# tn - topology_node
 
 	# Scheme это hash вида 
-			hosts = {
-						:main => { :model => nil, :database => nil, :app_serv => nil, :driver => nil },
-						:reserve => {:model => nil, :database => nil, :app_serv => nil, :driver => nil },
-						:arb => {:model => nil},
-						:client1 => {:model => nil}
-					}
-			
-}
-    def initialize hash
-    	hash.each_pair
+    def initialize hash 
+    	hash.each_pair do |key, value|
+    		create_node(key)
+    		next if value.nil?
+    		
+		end
 	end
+
+	def fill_host hash
+		hash.each_pair do |in_key, in_value|
+			case in_key
+			when :model
+				in_value = Host_model.new(topology_node_name,description,belongs,ip,mask, type)
+			when :database
+				in_value = Database_model.new(topology_node_name,description,belongs,protocol,port, resource, user, password)
+			when :app_serv
+				in_value = App_server_Model.new(topology_node_name, description, belongs, protocol, host, port, ntf_port)
+			when :driver
+				in_value = App_server_Model.new(topology_node_name, description, belongs, protocol, host, port, ntf_port)
+			else
+
+			end
+		end
+	end
+
 
     def tree_size
     	return 5
@@ -28,11 +59,23 @@ class TopologyTree
 	
 	end
 
-	def create_node
+	def create_node type_node
+		node = TopologyNode.new(type_node)
+		return node
+	end
+
+	def add_children_node
 
 	end
 
 	def delete_node
 
 	end
+
+	def show_tree
+
+	end
 end
+
+
+
